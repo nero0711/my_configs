ade7c22b4f15c61b19b3bb7dd9767749bd527902
nero0711 <alekov8@gmail.com>
2020-12-18 00:17:31 +0300

    +

--- vim/install.sh
+++ vim/install.sh
@@ -1,3 +1,4 @@
-cp config.vim ~/.vimrc
+# wget -O https://github.com/nero0711/my_configs/blob/main/vim/install.sb | bash
+[ -f config.vim ] && cp config.vim ~/.vimrc || wget -P https://github.com/nero0711/my_configs/blob/main/vim/config.vim && mv ./config.vim ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
